fc7d697b3183c56f404963eb9e67b3f74600377f
RATIVACRIS <rativachristian@gmail.com>
2023-01-05 16:20:46 -0500

    homework 03 terminada

--- M03 JavaScript Fundamentos/Ejercicio 04.js
+++ M03 JavaScript Fundamentos/Ejercicio 04.js
@@ -6,31 +6,49 @@
 function elevarAlCuadrado(num) {
    // Retorna el valor de "num" elevado al cuadrado.
    // Tu código:
+   return Math.pow (num,2);
 }
+elevarAlCuadrado (8)
 
 function elevarAlCubo(num) {
    // Retorna el valor de "num" elevado al cubo.
    // Tu código:
+   return Math.pow (num, 3);
 }
+elevarAlCubo (8)
 
 function elevar(num, exponent) {
    // Retorna el valor de "num" elevado al exponente "exponent".
    // Tu código:
+   return   Math.pow (num, exponent);
 }
+elevar (8, 2)
 
 function redondearNumero(num) {
    // Redondea "num" al entero más próximo y retórnalo.
    // Tu código:
+   var resto = num %1;
+   var entero = num-resto;
+   if (resto >= 0.5){
+      return entero +1;
+   } else {
+      return entero;
+   }
+   return Math.round (num);
 }
+redondearNumero (7.4)
 
 function redondearHaciaArriba(num) {
    // Redondea "num" hacia arriba y retórnalo.
    // Tu código:
+   return Math.ceil(num);
 }
+redondearHaciaArriba (12.5)
 
 function numeroRandom() {
    // Genera un número al azar entre 0 y 1 y retórnalo.
    // Tu código:
+   return Math.random();
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
